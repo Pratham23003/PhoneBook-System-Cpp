b59cf6acff42afd9923e4bed3ec29e158507aeaf
Pratham <pratham@192.168.1.8>
2025-01-19 23:36:12 +0530

    Updated Template

--- phoneBook.cpp
+++ phoneBook.cpp
@@ -22,14 +22,38 @@ void Phonebook :: addContact(){
     file.close();
 }
 void Phonebook :: showContacts(){
-
+//aarti da code
 }
 void Phonebook :: searchContact(){
-
+//aarti da code
 }
 
 int main(){
-    Phonebook obj1;
-    obj1.addContact();
+    char choice;
+    cout << "1 --> Add Contact ";
+    cout << "2 --> Show Contacts ";
+    cout << "3 --> Search Contact ";
+    cout << "4 --> Delete Contact ";
+    cout << "5 --> Exit ";
+
+    switch(choice){
+        case '1':
+
+        break;
+        case '2':
+        //aarti da code
+        break;
+        case '3':
+        //aryan da code
+        break;
+        case '4':
+        //saksham da code
+        break;
+        case '5':
+        //exit ala
+        break;
+    default:
+
+    }
     return 0;
 }
